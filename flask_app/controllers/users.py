56c40eb3f06f34675da4b5496ecb5c7518d0e766
David Hausmann <david_hausmann@aol.com>
2022-10-28 23:54:30 -0400

    Added all the weapons

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -116,3 +116,17 @@ def all_maps():
         }
         logged_in_user = user.User.get_user_by_id(data)
         return render_template('all_maps.html', logged_in_user=logged_in_user)
+
+
+@app.route('/dashboard/weapons/')
+def all_weapons():
+    if 'user_id' not in session:
+        return redirect('/')
+    else:
+        data = {
+            'id': session['user_id']
+        }
+        logged_in_user = user.User.get_user_by_id(data)
+        return render_template('all_weapons.html', logged_in_user=logged_in_user)
+
+
